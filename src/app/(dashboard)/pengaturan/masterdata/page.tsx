32eaaf56e576c4750435cdfe186f44e077a835ff
I Wayan Budiastra <wayan.budiastra26@gmail.com>
2026-05-11 19:11:49 +0800

    chore: update page.tsx

--- src/app/(dashboard)/pengaturan/masterdata/page.tsx
+++ src/app/(dashboard)/pengaturan/masterdata/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function MasterdataPage() {
-  redirect("/pengaturan/masterdata/poli");
+  redirect("/pengaturan/masterdata/tindakan");
 }
