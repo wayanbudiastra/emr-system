66b17f9c1348302dc54233de8d6bb8f42614ee93
I Wayan Budiastra <wayan.budiastra26@gmail.com>
2026-05-12 09:01:02 +0800

    chore: update JadwalPraktekTable.tsx

--- src/features/dokter/components/JadwalPraktekTable.tsx
+++ src/features/dokter/components/JadwalPraktekTable.tsx
@@ -66,7 +66,7 @@ export function JadwalPraktekTable({ dokterProfileId, poliMappings }: Props) {
         </div>
       )}
 
-      <Accordion type="multiple" defaultValue={aktifMappings.map(m => m.id)}>
+      <Accordion openMultiple defaultValue={aktifMappings.map(m => m.id)}>
         {aktifMappings.map(mapping => (
           <AccordionItem key={mapping.id} value={mapping.id}>
             <AccordionTrigger className="text-sm font-medium">
