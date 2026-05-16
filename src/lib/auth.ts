ec5c1a1eb3a77ea53f9cdb928feca59cfbef8e66
I Wayan Budiastra <wayan.budiastra26@gmail.com>
2026-05-16 20:06:17 +0800

    chore: update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
