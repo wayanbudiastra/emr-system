00e10aced5a06ba69620962fb1131d7b718aa996
I Wayan Budiastra <wayan.budiastra26@gmail.com>
2026-05-16 11:18:28 +0800

    chore: add prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,12 @@
 import 'server-only';
 import { PrismaClient } from "@prisma/client";
-import { createPrismaClient } from "@/lib/db";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prismaClient: PrismaClient | undefined;
-  // eslint-disable-next-line no-var
-  var prismaInit: Promise<PrismaClient> | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-if (!globalThis.prismaInit) {
-  globalThis.prismaInit = createPrismaClient(process.env.DATABASE_URL!).then(
-    (client) => {
-      globalThis.prismaClient = client;
-      return client;
-    }
-  );
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export async function getPrisma(): Promise<PrismaClient> {
-  return globalThis.prismaInit!;
+  return prisma;
 }
